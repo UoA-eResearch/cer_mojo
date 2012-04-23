389371ea9bcf7ac676e568fc2bb48f0a961e1f83
Martin Feller <martinfeller73@gmail.com>
2012-04-23 23:37:41 +1200

    add comments

--- python/cluster_rest/cluster_rest/views.py
+++ python/cluster_rest/cluster_rest/views.py
@@ -8,27 +8,32 @@ job = Service(name='job', path='/job/{id}')
 
 @nodes.get()
 def get_nodes(request):
+  ''' Get list of cluster node names '''
   nodelist = factory.create_nodes_instance().get_node_list()
   return { 'nodes': nodelist }
 
-
 @node.get()
 def get_node(request):
+  ''' Get information about a cluster node '''
   nodename = str(request.matchdict['id'])
   node = factory.create_node_instance(nodename).get_info()
   return { 'node': node }  
 
 @jobs.get()
 def get_jobs(request):
+  '''
+    Get list of jobs. If the querystring parameter uid is set
+    only jobs for the given user are returned
+  '''
   uid = ''
   if 'uid' in request.params:
     uid = str(request.params['uid'])
   jobs = factory.create_queue_instance().get_jobs(uid)
   return { 'jobs': jobs }
 
-
 @job.get()
 def get_job(request):
+  ''' Get information about a job '''
   jobid = str(request.matchdict['id'])
   job = factory.create_job_instance(jobid).get_info()
   return { 'job': job }
