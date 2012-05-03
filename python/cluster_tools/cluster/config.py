a265bbab4f9ee4401fa135f9c1c6a70ba006bb7b
Martin Feller <martinfeller73@gmail.com>
2012-05-03 14:45:02 +1200

    adding values for pbs (commented out)

--- python/cluster_tools/cluster/config.py
+++ python/cluster_tools/cluster/config.py
@@ -1,2 +1,4 @@
 default_scheduler = 'loadleveler'
 main_ganglia_page = 'http://xcat.ceres.auckland.ac.nz/ganglia/'
+# default_scheduler = 'pbs'
+# main_ganglia_page = 'http://cluster.ceres.auckland.ac.nz/ganglia/'
