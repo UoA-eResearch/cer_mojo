d08a57ca6c0896fc47b71798368748c1de30e7e9
Martin Feller <martinfeller73@gmail.com>
2013-08-22 15:42:30 +1200

    change gmond server and run ssh prefix

--- python/cluster_tools/cluster/config.py
+++ python/cluster_tools/cluster/config.py
@@ -1,6 +1,6 @@
-scheduler_command_prefix = 'ssh login1-p.auckland.nesi.org.nz'
+scheduler_command_prefix = 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /etc/apache2/ssh/id_rsa ganglia@10.0.101.204'
 default_scheduler = 'loadleveler_affinity'
 ganglia_main_page = 'http://ganglia.uoa.nesi.org.nz/?c=University%20of%20Auckland%20HPC%20Cluster'
-ganglia_gmond_aggregator = 'pan.nesi.org.nz'
+ganglia_gmond_aggregator = 'localhost'
 ganglia_gmond_port = '8649'
 ganglia_blacklist = [ 'login-p', 'xcat-p', 'gpfs-a3-001-p', 'gpfs-a3-002-p', 'gpfs-a3-003-p', 'gpfs-a3-004-p' ]
