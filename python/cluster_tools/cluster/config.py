f3ce51442196d445a8e7bbb959892e50f8e89e85
Martin Feller <martinfeller73@gmail.com>
2013-08-23 12:01:52 +1200

    add ganglia aggregator machine to blacklist

--- python/cluster_tools/cluster/config.py
+++ python/cluster_tools/cluster/config.py
@@ -3,4 +3,4 @@ default_scheduler = 'loadleveler_affinity'
 ganglia_main_page = 'http://ganglia.uoa.nesi.org.nz/?c=University%20of%20Auckland%20HPC%20Cluster'
 ganglia_gmond_aggregator = 'localhost'
 ganglia_gmond_port = '8649'
-ganglia_blacklist = [ 'login-p', 'xcat-p', 'gpfs-a3-001-p', 'gpfs-a3-002-p', 'gpfs-a3-003-p', 'gpfs-a3-004-p' ]
+ganglia_blacklist = [ 'pan-ganglia-p', 'login-p', 'xcat-p', 'gpfs-a3-001-p', 'gpfs-a3-002-p', 'gpfs-a3-003-p', 'gpfs-a3-004-p' ]
